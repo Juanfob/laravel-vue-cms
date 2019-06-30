a680c8c9b60ee65c5e92e633077b8ffdb12ef70a
Juan Fermin <juanfob@gmail.com>
2019-06-30 14:15:38 +0200

    Finalización del proyecto, funcionando ok

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,16 @@
 
 require('./bootstrap');
 
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Homepage from './components/Homepage'
+import Create from './components/Create'
+import Read from './components/Read'
+import Update from './components/Update'
+import Comments from './components/Comments'
+
+Vue.use(VueRouter);
+
 window.Vue = require('vue');
 
 /**
@@ -26,7 +36,32 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/admin/dashboard',
+            name: 'read',
+            component: Read,
+            props: true
+        },
+        {
+            path: '/admin/create',
+            name: 'create',
+            component: Create,
+            props: true
+        },
+        {
+            path: '/admin/update',
+            name: 'update',
+            component: Update,
+            props: true
+        },
+    ],
+});
 
 const app = new Vue({
     el: '#app',
+    router,
+    components: { Homepage, Comments},
 });
